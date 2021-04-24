fe10d70eb172584b8b3ff4435d7202f37d658f12
Lounes Brahimi <lounesnewgs5@gmail.com>
2021-04-24 08:59:15 +0200

    bdd : crypto

--- bdd_crypto_adviser.sql
+++ bdd_crypto_adviser.sql
@@ -29,3 +29,12 @@ CREATE TABLE  bdd_crypto_adviser.Report (
 	 created_at DATETIME NOT NULL ,
 	 PRIMARY KEY ( id_report )
 ) ENGINE = INNODB;
+
+CREATE TABLE  bdd_crypto_adviser.Crypto (
+	 Crypto_id INT( 11 ) NOT NULL AUTO_INCREMENT ,
+	 Name VARCHAR( 60 ) NOT NULL ,
+	 price DECIMAL( 11 ) NOT NULL,
+	 last_update DATETIME NOT NULL ,
+	 PRIMARY KEY ( Crypto_id )
+) ENGINE = INNODB;
+

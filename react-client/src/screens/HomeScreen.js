0d4131d98ea0f3dfe07e2bfcce9e5cca3e39b071
edwinans <edyansari5@gmail.com>
2021-05-30 14:53:19 +0200

    [Client]: fetch board data

--- react-client/src/screens/HomeScreen.js
+++ react-client/src/screens/HomeScreen.js
@@ -10,22 +10,25 @@ class HomeScreen extends Component {
         super(props)
         this.state = {
             menuOpen: false,
+            loading: true,
+            items: [],
         }
         // console.log('this.props.history', this.props.history)
     }
 
-    componentDidMount() {
-
-        // this.isDesktop = helpers.isDesktop()
-        // console.log('this.isDesktop', this.isDesktop)
+    fetchData() {
+        alert("called")
+        this.setState({
+            loading: true
+        })
         fetch("http://localhost:8080/crypto-adviser/board")
             .then(res => res.json())
             .then(
                 (result) => {
                     console.log(result);
                     this.setState({
-                        isLoaded: true,
-                        items: result.items
+                        loading: false,
+                        items: result.data
                     });
                 },
                 // Note: it's important to handle errors here
@@ -33,15 +36,41 @@ class HomeScreen extends Component {
                 // exceptions from actual bugs in components.
                 (error) => {
                     this.setState({
-                        isLoaded: true,
+                        loading: false,
                         error
                     });
                 }
             )
     }
 
+    componentDidMount() {
+        setInterval(this.fetchData(), 3000)
+
+        // this.isDesktop = helpers.isDesktop()
+        // console.log('this.isDesktop', this.isDesktop)
+
+    }
 
 
+    // data": [
+    //     {
+    //       "id": "90",
+    //       "symbol": "BTC",
+    //       "name": "Bitcoin",
+    //       "nameid": "bitcoin",
+    //       "rank": 1,
+    //       "price_usd": "36245.72",
+    //       "percent_change_24h": "3.51",
+    //       "percent_change_1h": "1.23",
+    //       "percent_change_7d": "-2.39",
+    //       "price_btc": "1.00",
+    //       "market_cap_usd": "676434039172.47",
+    //       "volume24": 49858935678.40042,
+    //       "volume24a": 55812760456.74331,
+    //       "csupply": "18662452.00",
+    //       "tsupply": "18662452",
+    //       "msupply": "21000000"
+    //     }
     render() {
         return (
             <div className="wrapper home">
@@ -49,72 +78,66 @@ class HomeScreen extends Component {
 
                 <div className="main-container">
                     <Container>
-                        <Row className="mt-5">
-                            <Col>
-                                <h3 className="text-white text-center mb-3">
-                                    Live currency prices
-                                </h3>
-                                <Table striped bordered hover size="sm" className="bg-white">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>
-                                                Coin
-                                            </th>
-                                            <th>Price</th>
-                                            <th>Market</th>
-                                            <th>24h volume</th>
-                                            <th>1h</th>
-                                            <th>24h</th>
-                                            <th>7d</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>
-                                                <Link to={"/coins/{id}"}>
-                                                    Bitcoin
-                                                </Link>
-                                            </td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                            <td>@mdo</td>
-                                            <td>@mdo</td>
-                                            <td>@mdo</td>
-                                            <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>
-                                                <Link to={"/coins/{id}"}>
-                                                    Bitcoin
-                                                </Link>
-                                            </td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                            <td>@fat</td>
-                                            <td>@fat</td>
-                                            <td>@fat</td>
-                                            <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td colSpan="2">Larry the
-                                                <Link to={"/coins/{id}"}>
-                                                    Bitcoin
-                                                </Link>
-                                            </td>
-                                            <td>@twitter</td>
-                                            <td>@twitter</td>
-                                            <td>@twitter</td>
-                                            <td>@twitter</td>
-                                            <td>@twitter</td>
-                                        </tr>
-                                    </tbody>
-                                </Table>
-                            </Col>
-                        </Row>
+                        {
+                            this.state.loading ? <p>loading ...</p> :
+                                <Row className="mt-5">
+                                    <Col>
+                                        <h3 className="text-white text-center mb-3">
+                                            Live currency prices
+                                        </h3>
+                                        <Table striped bordered hover size="sm" className="bg-white">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>
+                                                        Coin
+                                                    </th>
+                                                    <th>Price</th>
+                                                    <th>Market</th>
+                                                    <th>24h volume</th>
+                                                    <th>1h</th>
+                                                    <th>24h</th>
+                                                    <th>7d</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {
+                                                    this.state.items && this.state.items.map((item, i) =>
+                                                        <tr>
+                                                            <td>{i + 1}</td>
+                                                            <td>
+                                                                <Link to={`/coins/${item.id}`}>
+                                                                    {item.name}
+                                                                </Link>
+                                                            </td>
+                                                            <td>
+                                                                    {item.price_usd + " $"}
+                                                            </td>
+                                                            <td>
+                                                                {item.market_cap_usd + " $"}
+                                                            </td>
+                                                            <td>
+                                                                {item.volume24 + " $"}
+                                                            </td>
+                                                            <td>
+                                                                {item.percent_change_1h + "%"}
+                                                            </td>
+                                                            <td>
+                                                                {item.percent_change_24h + "%"}
+                                                            </td>
+                                                            <td>
+                                                                {item.percent_change_7d + "%"}
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                                }
+
+                                            </tbody>
+                                        </Table>
+                                    </Col>
+                                </Row>
+                        }
+
                     </Container>
                 </div>
 
